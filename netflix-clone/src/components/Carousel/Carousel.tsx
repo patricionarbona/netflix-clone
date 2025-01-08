ab98208d9cfa8ca06ec68aef2f7ae2b6009d0d33
patricionarbona <patricioinarbona@gmail.com>
2025-01-08 20:33:17 +0100

    add style slider carosuel

--- netflix-clone/src/components/Carousel/Carousel.tsx
+++ netflix-clone/src/components/Carousel/Carousel.tsx
@@ -1,84 +1,124 @@
-import { useEffect, useState } from "react"
-import { fetchPages } from "../../services/fetchs"
-import './Carousel.css'
+import React, { useEffect, useRef, useState } from "react";
+import { fetchPages } from "../../services/fetchs";
+import "./Carousel.css";
 
 interface Movie {
-    adult: boolean
-    backdrop_path: string
-    genre_ids: number[]
-    id: number
-    original_language: string
-    original_title: string
-    overview: string
-    popularity: number
-    poster_path: string
-    release_date: string
-    title: string
-    video: boolean
-    vote_average: number
-    vote_count: number
+  adult: boolean;
+  backdrop_path: string;
+  genre_ids: number[];
+  id: number;
+  original_language: string;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  release_date: string;
+  title: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
 }
 
 interface FetchMovies {
-    page: number
-    results: Movie[]
+  page: number;
+  results: Movie[];
 }
 
-const urlGenre = 'https://api.themoviedb.org/3/discover/movie'
-const urlPoster = 'https://image.tmdb.org/t/p/original/'
-
+const urlGenre = "https://api.themoviedb.org/3/discover/movie";
+const urlPoster = "https://image.tmdb.org/t/p/original/";
 
 const paramsLanguage = new URLSearchParams({
-    language: 'es-ES',
-    sort_by: 'popularity.desc'
-})
+  language: "es-ES",
+  sort_by: "popularity.desc",
+});
 
-export const Carousel = ({genre_id, name}: {genre_id:number, name:string}) => {
-    const [movies, setMovies] = useState<FetchMovies[]>([])
-    const [loading, setLoading] = useState<boolean>(true)
-    const [error, setError] = useState<string | null>(null)
+export const Carousel = ({
+  genre_id,
+  name,
+}: {
+  genre_id: number;
+  name: string;
+}) => {
+  const [movies, setMovies] = useState<FetchMovies[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
+  // states del carousel
+  const [position, setPosition] = useState(0);
+  const [offset, setOffSet] = useState(0);
+  const carouselRef = useRef<HTMLDivElement>(null);
 
-    useEffect(() => {
-        paramsLanguage.append('with_genres', String(genre_id))
-        paramsLanguage.append('primary_release_date.gte', '2024-01-01');
-        paramsLanguage.append('primary_release_date.lte', '2024-12-31');
+  const handleNext = () => {
+    console.log("next");
+    console.log(position);
+    if (carouselRef.current) {
+      const item = carouselRef.current.querySelector(
+        ".carousel-slider"
+      ) as HTMLElement;
+      if (item) {
+        setOffSet((prevOffset) => prevOffset - 100);
+        setPosition((position) => position + 1);
+      }
+    }
+  };
 
-        const fetchData = async () => {
-            try {
-                const data = await fetchPages<FetchMovies>(urlGenre, paramsLanguage, 2)
-                setMovies(data)
-                setLoading(false)
-            } catch (error) {
-                setError(`Error al cargar las peliculas: ${error}`)
-            } finally {
-                setLoading(false)
-            }   
-        }
+  const handlePrev = () => {
+    console.log("prev");
+    console.log(position);
+    if (carouselRef.current) {
+      const item = carouselRef.current.querySelector(
+        ".carousel-slider"
+      ) as HTMLElement;
+      if (item) {
+        setOffSet((prevOffset) => prevOffset + 100);
+        setPosition((position) => position - 1);
+      }
+    }
+  };
 
-        fetchData()
+  useEffect(() => {
+    paramsLanguage.append("with_genres", String(genre_id));
+    paramsLanguage.append("primary_release_date.gte", "2024-01-01");
+    paramsLanguage.append("primary_release_date.lte", "2024-12-31");
 
-    }, [genre_id])
+    const fetchData = async () => {
+      try {
+        const data = await fetchPages<FetchMovies>(urlGenre, paramsLanguage, 2);
+        setMovies(data);
+        setLoading(false);
+      } catch (error) {
+        setError(`Error al cargar las peliculas: ${error}`);
+      } finally {
+        setLoading(false);
+      }
+    };
 
-    if(loading) {return <div>Loading...</div>}
-    if(error) {return <div>{error}</div>}
+    fetchData();
+  }, [genre_id]);
 
-    return(
-        <div className="carousel">
-            <h3 className="carousel-name">{name}</h3>
-            <div className="carousel-items"
-                style={{ "--quantity": movies.length } as React.CSSProperties}
-            >
-                <ul className="carousel-list">
-                    {movies && movies.map(movieResponse => (
-                        movieResponse.results.map(movie => (
-                            <li key={movie.id} className="carousel-item"><img className="carousel-img" src={`${urlPoster + movie.backdrop_path}`} alt={movie.title} /></li>
-                        ))
-                    ))}
-                </ul>
-            </div>
-            <div className="carousel-arrows">
-                    <button>&lt;</button>
-                    <button>&gt;</button>
-            </div>
-        </div>
-)} 
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+  if (error) {
+    return <div>{error}</div>;
+  }
+
+  return (
+    <div className="carousel-container" ref={carouselRef}>
+      <button className="carousel-handle carousel-left-handle" onClick={handlePrev}>
+        <div className="text">&#8249;</div>
+      </button>
+      <div className="carousel-slider"
+      style={{
+        transform: `translateX(${offset}%)`
+      }}>
+        {movies.map((moviesResponse) => 
+          moviesResponse.results.map((movie) => (
+            <img key={`${genre_id}-slider-${movie.id}`} src={urlPoster + movie.backdrop_path}/>
+          )))}
+      </div>
+      <button className="carousel-handle carousel-right-handle" onClick={handleNext}>
+        <div className="carousel-text">&#8250;</div>
+      </button>
+    </div>
+  );
+};
