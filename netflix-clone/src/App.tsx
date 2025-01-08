d090914fbdab5b6c4516b55253d0759acccb19c5
patricionarbona <patricioinarbona@gmail.com>
2025-01-08 20:33:36 +0100

    chore clear styles

--- netflix-clone/src/App.tsx
+++ netflix-clone/src/App.tsx
@@ -1,12 +1,39 @@
+import { useEffect, useState } from 'react'
 import './App.css'
 import { Banner, Carousel, Nav } from "./components"
+import { fetchMovieGenres } from './services/fetchs'
+
+interface Genero {
+  id: number;
+  name: string;
+}
 
 function App() {
+  const [generos, setGeneros] = useState<Genero[]>([])
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await fetchMovieGenres();
+        setGeneros(data);
+      } catch (error) {
+        console.error("Error al obtener géneros:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <>
-    <Nav></Nav>
+    {/* <Nav></Nav> */}
     {/* <Banner/> */}
-    <Carousel genre_id={35} />
+
+    {/* {generos && generos.map(genero =>(
+      <Carousel key={genero.id} genre_id={genero.id} />
+    ))} */}
+
+    <Carousel genre_id={35} name='Acción' />
     </>
   )
 }
