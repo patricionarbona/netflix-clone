9cac16e01d5c0aed0d8dbb16e41ccd9c4beac078
patricionarbona <patricioinarbona@gmail.com>
2025-01-24 13:45:41 +0100

    feat add hoverModal

--- netflix-clone/src/components/modals/HoverModal.tsx
+++ netflix-clone/src/components/modals/HoverModal.tsx
@@ -1,8 +1,101 @@
-import './HoverModal.css'
+import { useEffect, useState } from "react";
+import { fetchMovieVideos } from "../../services/fetchs";
+import { ButtonsItemControls } from "../Buttons/ButtonsItemControls";
+import { GenresList } from "../Carousel/GenresList";
+import { VideoContainer } from "../Video/VideoContainer";
+import "./HoverModal.css";
 
-export const HoverModal = () => {
-    return (
-        <>
-        </>
-    )
+interface Movie {
+  adult: boolean;
+  backdrop_path: string;
+  genre_ids: number[];
+  id: number;
+  original_language: string;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  release_date: string;
+  title: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
 }
+
+interface Genero {
+  id: number;
+  name: string;
+}
+
+interface VideoMovie {
+  iso_639_1: string;
+  iso_3166_1: string;
+  name: string;
+  key: string;
+  site: string;
+  size: number;
+  type: string;
+  official: boolean;
+  published_at: string;
+  id: string;
+}
+
+const urlPoster = "https://image.tmdb.org/t/p/original/";
+
+export const HoverModal = ({
+  movie,
+  genres,
+}: {
+  movie: Movie;
+  genres: Genero[];
+}) => {
+  const movieGenres = genres.filter((genero) =>
+    movie.genre_ids.includes(genero.id)
+  );
+
+  const [videos, setVideos] = useState<VideoMovie[]>([]);
+  const [showVideo, setShowVideo] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await fetchMovieVideos(movie.id);
+        setVideos(data.filter((video) => video.type === "Teaser"));
+      } catch (error) {
+        console.error("Error al obtener videos:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowVideo(true);
+    }, 3000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  return (
+    <div className="hoverModal">
+      <div className="hoverModal-display-container">
+        {showVideo ? (
+          <VideoContainer
+            route={videos[0]?.key}
+            banner={false}
+            className={showVideo ? "show" : "hidden"}
+          />
+        ) : (
+          <img
+            src={urlPoster + movie.poster_path}
+            alt=""
+            className={showVideo ? "hidden" : "show"}
+          />
+        )}
+      </div>
+      <ButtonsItemControls />
+      <GenresList genres={movieGenres} />
+    </div>
+  );
+};
