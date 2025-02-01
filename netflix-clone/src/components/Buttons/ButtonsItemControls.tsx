54fae1847664fbbc5d7a92d9e1a3cda82e3cb64a
patricionarbona <patricioinarbona@gmail.com>
2025-02-01 19:09:45 +0100

    refactor ItemControls

--- netflix-clone/src/components/Buttons/ButtonsItemControls.tsx
+++ netflix-clone/src/components/Buttons/ButtonsItemControls.tsx
@@ -1,5 +1,5 @@
 import "./ButtonsItemControls.css";
-import { LikeGroupButton, ButtonAddList, ButtonPlayCirc } from "../../components";
+import { LikeGroupButton, ButtonAddList, ButtonPlayCirc, ButtonArrowDown } from "../../components";
 
 export const ButtonsItemControls = () => {
   return (
@@ -10,22 +10,7 @@ export const ButtonsItemControls = () => {
           <LikeGroupButton />
         </div>
         <div className="moreInfo">
-          <button>
-            <span className="tooltip">Episodios e información</span>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke-width="1.5"
-              stroke="currentColor"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                d="m19.5 8.25-7.5 7.5-7.5-7.5"
-              />
-            </svg>
-          </button>
+          <ButtonArrowDown tooltip={true} tooltipText="Episodios e información" />
         </div>
       </div>
   );
