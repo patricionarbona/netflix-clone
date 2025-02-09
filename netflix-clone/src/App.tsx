1f5a8abc3db86d8d17f6044107ef57aa28639222
patricionarbona <patricioinarbona@gmail.com>
2025-02-09 17:03:02 +0100

    chore prototype app

--- netflix-clone/src/App.tsx
+++ netflix-clone/src/App.tsx
@@ -1,28 +1,15 @@
-import { useEffect, useState } from "react";
 import "./App.css";
-import { Carousel, Nav, VideoContainer } from "./components";
-import { fetchMovieGenres } from "./services/fetchs";
-
-interface Genero {
-  id: number;
-  name: string;
-}
+import {
+  Carousel,
+  DisplayContentModal,
+  HoverModal,
+  Nav,
+  VideoContainer,
+} from "./components";
+import { useGlobalContext } from "./context/global.context";
 
 function App() {
-  const [generos, setGeneros] = useState<Genero[]>([]);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const data = await fetchMovieGenres();
-        setGeneros(data);
-      } catch (error) {
-        console.error("Error al obtener géneros:", error);
-      }
-    };
-
-    fetchData();
-  }, []);
+  const { generos, showHover, isModalOpen } = useGlobalContext();
 
   const ejemplo = {
     iso_639_1: "es",
@@ -39,45 +26,47 @@ function App() {
 
   return (
     <>
-      {/* <Nav></Nav> */}
+      <Nav></Nav>
 
       <VideoContainer route={ejemplo.key} banner={true} />
       <div className="container-carousels">
-
-      {generos && generos.length > 0 && (
-        <>
-          <Carousel genre_id={35} name={`Películas de ${generos[0].name}`} />
-          <Carousel
-            genre_id={generos[1].id}
-            name={`Películas de ${generos[1].name}`}
-          />
-          <Carousel
-            genre_id={generos[2].id}
-            name={`Películas de ${generos[2].name}`}
-          />
-          <Carousel
-            genre_id={generos[3].id}
-            name={`Películas de ${generos[3].name}`}
-          />
-          <Carousel
-            genre_id={generos[4].id}
-            name={`Películas de ${generos[4].name}`}
-          />
-          <Carousel
-            genre_id={generos[5].id}
-            name={`Películas de ${generos[5].name}`}
-          />
-          <Carousel
-            genre_id={generos[6].id}
-            name={`Películas de ${generos[6].name}`}
-          />
-          <Carousel
-            genre_id={generos[7].id}
-            name={`Películas de ${generos[7].name}`}
-          />
-        </>
-      )}
+        {generos && generos.length > 0 && (
+          <>
+            <Carousel genre_id={35} name={`Películas de ${generos[0].name}`} />
+            <Carousel
+              genre_id={generos[1].id}
+              name={`Películas de ${generos[1].name}`}
+            />
+            <Carousel
+              genre_id={generos[2].id}
+              name={`Películas de ${generos[2].name}`}
+            />
+            <Carousel
+              genre_id={generos[3].id}
+              name={`Películas de ${generos[3].name}`}
+            />
+            <Carousel
+              genre_id={generos[4].id}
+              name={`Películas de ${generos[4].name}`}
+            />
+            <Carousel
+              genre_id={generos[5].id}
+              name={`Películas de ${generos[5].name}`}
+            />
+            <Carousel
+              genre_id={generos[6].id}
+              name={`Películas de ${generos[6].name}`}
+            />
+            <Carousel
+              genre_id={generos[7].id}
+              name={`Películas de ${generos[7].name}`}
+            />
+          </>
+        )}
       </div>
+
+      {showHover && <HoverModal />}
+      {isModalOpen && <DisplayContentModal />}
     </>
   );
 }
