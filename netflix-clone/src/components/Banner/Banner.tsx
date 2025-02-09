cbb6cd8cae83ec2bfb094dde0e23dd7abe634d6c
patricionarbona <patricioinarbona@gmail.com>
2025-02-09 17:13:49 +0100

    chore refactor pretier

--- netflix-clone/src/components/Banner/Banner.tsx
+++ netflix-clone/src/components/Banner/Banner.tsx
@@ -1,96 +1,97 @@
-import { useEffect, useState } from 'react'
-import { useFetch } from '../../hook'
-import './banner.css'
+import { useEffect, useState } from "react";
+import { useFetch } from "../../hook";
+import "./banner.css";
 
 interface Movie {
-    adult: boolean
-    backdrop_path: string
-    genre_ids: number[]
-    id: number
-    original_language: string
-    original_title: string
-    overview: string
-    popularity: number
-    poster_path: string
-    release_date: string
-    title: string
-    video: boolean
-    vote_average: number
-    vote_count: number
+  adult: boolean;
+  backdrop_path: string;
+  genre_ids: number[];
+  id: number;
+  original_language: string;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  release_date: string;
+  title: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
 }
 
 interface Video {
-    iso_639_1: string       
-    iso_3166_1: string      
-    name: string            
-    key: string             
-    site: string            
-    size: number            
-    type: string            
-    official: boolean       
-    published_at: string    
-    id: string              
-  }
+  iso_639_1: string;
+  iso_3166_1: string;
+  name: string;
+  key: string;
+  site: string;
+  size: number;
+  type: string;
+  official: boolean;
+  published_at: string;
+  id: string;
+}
 
 interface fetchPopularMovie {
-    page: number
-    results: Movie[]
+  page: number;
+  results: Movie[];
 }
 
 interface fetchVideosMovie {
-    id:number
-    results: Video[]
+  id: number;
+  results: Video[];
 }
 
-const urlPopularMovies = 'https://api.themoviedb.org/3/movie/popular'
+const urlPopularMovies = "https://api.themoviedb.org/3/movie/popular";
 
 const paramsLanguage = new URLSearchParams({
-    language: 'es-ES'
-})
+  language: "es-ES",
+});
 
-const claveApi = import.meta.env.VITE_API_KEY
+const claveApi = import.meta.env.VITE_API_KEY;
 
 const options = {
-    method: 'GET',
-    headers: {
-        accept: 'application/json',
-        Authorization: `Bearer ${claveApi}`
-    }
-}
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization: `Bearer ${claveApi}`,
+  },
+};
 
 export const Banner = () => {
-    const [dataVideosMovie, setDataVideosMovies] = useState<fetchVideosMovie | null>(null)
-    const {data} = useFetch<fetchPopularMovie>(urlPopularMovies, paramsLanguage)
+  const [dataVideosMovie, setDataVideosMovies] =
+    useState<fetchVideosMovie | null>(null);
+  const { data } = useFetch<fetchPopularMovie>(
+    urlPopularMovies,
+    paramsLanguage
+  );
 
-    const fetchVideos = async (url: URL, params: any) => {
-        const response = await fetch(url, params)
-        const responseJson = await response.json()
-        setDataVideosMovies(responseJson)
+  const fetchVideos = async (url: URL, params: any) => {
+    const response = await fetch(url, params);
+    const responseJson = await response.json();
+    setDataVideosMovies(responseJson);
+  };
+
+  useEffect(() => {
+    if (data) {
+      const peli = data.results[0];
+      if (peli?.id) {
+        const urlVideosMovie = new URL(
+          `https://api.themoviedb.org/3/movie/${peli.id}/videos`
+        );
+        urlVideosMovie.search = paramsLanguage.toString();
+        fetchVideos(urlVideosMovie, options);
+      }
     }
+  }, [data]);
 
-    useEffect(() => {
-        if (data) {
-            const peli = data.results[0]
-            if (peli?.id) {
-                const urlVideosMovie = new URL(`https://api.themoviedb.org/3/movie/${peli.id}/videos`)
-                urlVideosMovie.search = paramsLanguage.toString()
-                fetchVideos(urlVideosMovie, options)
-            }
-        }
-    }, [data])
-
-
-    return (
-        <div className='banner'>
-            {dataVideosMovie
-                ? dataVideosMovie.results
-                    .filter((video) => video.type === 'Trailer')
-                    .map((video) => (
-                        <div key={video.id}>
-                            {video.key}
-                        </div>
-                    ))
-                : null}
-        </div>
-
-)}
+  return (
+    <div className="banner">
+      {dataVideosMovie
+        ? dataVideosMovie.results
+            .filter((video) => video.type === "Trailer")
+            .map((video) => <div key={video.id}>{video.key}</div>)
+        : null}
+    </div>
+  );
+};
