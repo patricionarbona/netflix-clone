f6e2f84802aeeaa6116fc157a7059c3fd1c32baf
patricionarbona <patricioinarbona@gmail.com>
2025-02-09 17:15:38 +0100

    chore refactor pretier

--- netflix-clone/src/components/Carousel/GenresList.tsx
+++ netflix-clone/src/components/Carousel/GenresList.tsx
@@ -8,20 +8,21 @@ interface Genero {
 export const GenresList = ({ genres }: { genres: Genero[] }) => {
   return (
     <>
-        <div className="genresList">
-          {genres.map((genero, index) => (
-            <>
-                {index !== 0 ? 
-                    <span
-                        key={`genreList-separato${index}`}
-                        className="genreList-separator"
-                    ></span>
-                    : <></>
-                }
-              <span key={`genreList-item-${index}`}>{genero.name}</span>
-            </>
-          ))}
-        </div>
+      <div className="genresList">
+        {genres.map((genero, index) => (
+          <>
+            {index !== 0 ? (
+              <span
+                key={`genreList-separato${index}`}
+                className="genreList-separator"
+              ></span>
+            ) : (
+              <></>
+            )}
+            <span key={`genreList-item-${index}`}>{genero.name}</span>
+          </>
+        ))}
+      </div>
     </>
   );
 };
