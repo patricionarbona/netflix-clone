e0a55d2f75405f0bebb30e1384dceed1a4f6b85c
patricionarbona <patricioinarbona@gmail.com>
2025-02-22 18:15:41 +0100

    refactor btnClose

--- netflix-clone/src/components/Buttons/ButtonClose.tsx
+++ netflix-clone/src/components/Buttons/ButtonClose.tsx
@@ -1,4 +1,4 @@
-import "./ButtonClose.css";
+import { ButtonCircle } from "./CircleButtons/ButtonCircle";
 
 interface ButtoncloseProps {
   tooltip?: boolean;
@@ -6,29 +6,34 @@ interface ButtoncloseProps {
   onClick?: () => void;
 }
 
+const svgClose = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    stroke-width="1.5"
+    stroke="currentColor"
+  >
+    <path
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      d="M6 18 18 6M6 6l12 12"
+    />
+  </svg>
+);
+
 export const ButtonClose: React.FC<ButtoncloseProps> = ({
   tooltip = false,
   tooltipText = "",
   onClick,
 }) => {
   return (
-    <div className="btn-close">
-      <button onClick={onClick}>
-        {tooltip && <span className="tooltip">{tooltipText}</span>}
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke-width="1.5"
-          stroke="currentColor"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            d="M6 18 18 6M6 6l12 12"
-          />
-        </svg>
-      </button>
-    </div>
+    <ButtonCircle
+      svgProp={svgClose}
+      addClass="btn-close"
+      textTooltip={tooltipText}
+      onClick={onClick}
+      showTooltip={tooltip}
+    />
   );
 };
