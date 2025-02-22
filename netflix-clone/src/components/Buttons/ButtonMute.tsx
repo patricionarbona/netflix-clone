250518b5c584843be65fab8c2822224d0b94f2ff
patricionarbona <patricioinarbona@gmail.com>
2025-02-22 18:21:00 +0100

    refactor mute

--- netflix-clone/src/components/Buttons/ButtonMute.tsx
+++ netflix-clone/src/components/Buttons/ButtonMute.tsx
@@ -1,23 +1,23 @@
-import "./ButtonMute.css";
+import { ButtonCircle } from "./CircleButtons/ButtonCircle";
 
-export const ButtonMute = ({onClick}: {onClick: () => void}) => {
+const svgMute = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    stroke-width="1.5"
+    stroke="currentColor"
+  >
+    <path
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      d="M17.25 9.75 19.5 12m0 0 2.25 2.25M19.5 12l2.25-2.25M19.5 12l-2.25 2.25m-10.5-6 4.72-4.72a.75.75 0 0 1 1.28.53v15.88a.75.75 0 0 1-1.28.53l-4.72-4.72H4.51c-.88 0-1.704-.507-1.938-1.354A9.009 9.009 0 0 1 2.25 12c0-.83.112-1.633.322-2.396C2.806 8.756 3.63 8.25 4.51 8.25H6.75Z"
+    />
+  </svg>
+);
+
+export const ButtonMute = ({ onClick }: { onClick: () => void }) => {
   return (
-    <div className="btn btn-mute" onClick={onClick}>
-      <button>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke-width="1.5"
-          stroke="currentColor"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            d="M17.25 9.75 19.5 12m0 0 2.25 2.25M19.5 12l2.25-2.25M19.5 12l-2.25 2.25m-10.5-6 4.72-4.72a.75.75 0 0 1 1.28.53v15.88a.75.75 0 0 1-1.28.53l-4.72-4.72H4.51c-.88 0-1.704-.507-1.938-1.354A9.009 9.009 0 0 1 2.25 12c0-.83.112-1.633.322-2.396C2.806 8.756 3.63 8.25 4.51 8.25H6.75Z"
-          />
-        </svg>
-      </button>
-    </div>
+    <ButtonCircle svgProp={svgMute} addClass="btn-mute" onClick={onClick} showTooltip={false}/>
   );
 };
