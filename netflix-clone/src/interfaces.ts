3669b5f9b3e434a0c57e26b17c3b91120f7df002
patricionarbona <patricioinarbona@gmail.com>
2025-02-23 18:57:53 +0100

    feat add  TVShow

--- netflix-clone/src/interfaces.ts
+++ netflix-clone/src/interfaces.ts
@@ -35,3 +35,20 @@ export interface ButtonCircleProps {
   svgProp: React.ReactNode;
   onClick?: () => void;
 }
+
+export interface TVShow {
+  adult: boolean;
+  backdrop_path: string;
+  genre_ids: number[];
+  id: number;
+  origin_country: string[];
+  original_language: string;
+  original_name: string;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  first_air_date: string;
+  name: string;
+  vote_average: number;
+  vote_count: number;
+}
