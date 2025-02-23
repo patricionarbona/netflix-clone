4ea67c11858fa2bd42897df1a569b27f99aa75f7
patricionarbona <patricioinarbona@gmail.com>
2025-02-23 18:57:33 +0100

    fix property name

--- netflix-clone/src/components/Buttons/ButtonClose.tsx
+++ netflix-clone/src/components/Buttons/ButtonClose.tsx
@@ -11,12 +11,12 @@ const svgClose = (
     xmlns="http://www.w3.org/2000/svg"
     fill="none"
     viewBox="0 0 24 24"
-    stroke-width="1.5"
+    strokeWidth="1.5"
     stroke="currentColor"
   >
     <path
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeLinecap="round"
+      strokeLinejoin="round"
       d="M6 18 18 6M6 6l12 12"
     />
   </svg>
