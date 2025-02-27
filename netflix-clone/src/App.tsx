2c2cc433a012c0f0d71a9ec3d1c90f91a487b91e
patricionarbona <patricioinarbona@gmail.com>
2025-02-27 19:42:14 +0100

    feat change use edited carousel component

--- netflix-clone/src/App.tsx
+++ netflix-clone/src/App.tsx
@@ -17,44 +17,17 @@ function App() {
 
       <Banner />
       <div className="container-carousels">
-        {generos && generos.length > 0 && (
+        {generos && generos.movies.length > 0 && (
           <>
-            <Carousel genre_id={35} name={`Películas de ${generos[0].name}`} />
-            <Carousel
-              genre_id={generos[1].id}
-              name={`Películas de ${generos[1].name}`}
-            />
-            <Carousel
-              genre_id={generos[2].id}
-              name={`Películas de ${generos[2].name}`}
-            />
-            <Carousel
-              genre_id={generos[3].id}
-              name={`Películas de ${generos[3].name}`}
-            />
-            <Carousel
-              genre_id={generos[4].id}
-              name={`Películas de ${generos[4].name}`}
-            />
-            <Carousel
-              genre_id={generos[5].id}
-              name={`Películas de ${generos[5].name}`}
-            />
-            <Carousel
-              genre_id={generos[6].id}
-              name={`Películas de ${generos[6].name}`}
-            />
-            <Carousel
-              genre_id={generos[7].id}
-              name={`Películas de ${generos[7].name}`}
-            />
+            <Carousel genre_id={35} textHeader={`Películas de ${generos.movies[0].name}`} isSerie={false} />
+            <Carousel genre_id={35} textHeader={`Películas de ${generos.tv[0].name}`} isSerie={true} />
+            <Carousel genre_id={35} textHeader={`Películas de ${generos.tv[1].name}`} isSerie={true} isPopular={true} />
           </>
         )}
       </div>
 
       {showHover && !isResizing && <HoverModal />}
       {isModalOpen && <DisplayContentModal />}
-      {/* <script src="https://www.youtube.com/iframe_api"></script> */}
     </>
   );
 }
