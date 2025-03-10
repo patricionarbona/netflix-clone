7ffa88925f5e7dad2eb60cef6846e2c04c4b0df3
patricionarbona <patricioinarbona@gmail.com>
2025-03-10 13:51:16 +0100

    feat map tv genres

--- netflix-clone/src/components/pages/SeriesPage.tsx
+++ netflix-clone/src/components/pages/SeriesPage.tsx
@@ -1,8 +1,26 @@
+import { useGlobalContext } from "../../context/global.context";
+import { Banner } from "../Banner/Banner";
+import { Carousel } from "../Carousel/Carousel";
+
 export const SeriesPage = () => {
+  const { generos } = useGlobalContext();
+
   return (
     <>
-      <div>
-        <p>Series</p>
+      <Banner />
+      <div className="container-carousels">
+        {generos &&
+          generos.tv.map((genero, index) => (
+            <Carousel
+              key={`tv-${index}-${genero.name}`}
+              genre_id={genero.id}
+              textHeader={`Series ${
+                index !== 0 && index % 3 === 0 ? "populares" : ""
+              } de ${genero.name}`}
+              isSerie={true}
+              isPopular={index !== 0 && index % 3 === 0}
+            />
+          ))}
       </div>
     </>
   );
