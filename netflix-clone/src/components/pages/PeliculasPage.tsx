bb6d6fea128879afd370b836035e65238cfa5d71
patricionarbona <patricioinarbona@gmail.com>
2025-03-10 13:54:14 +0100

    feat map movies

--- netflix-clone/src/components/pages/PeliculasPage.tsx
+++ netflix-clone/src/components/pages/PeliculasPage.tsx
@@ -1,10 +1,27 @@
+import { useGlobalContext } from "../../context/global.context";
+import { Banner } from "../Banner/Banner";
+import { Carousel } from "../Carousel/Carousel";
+
 export const PeliculasPage = () => {
-    return (
-      <>
-        <div>
-          <p>Peliculas</p>
-        </div>
-      </>
-    );
-  };
-  
+  const { generos } = useGlobalContext();
+
+  return (
+    <>
+      <Banner />
+      <div className="container-carousels">
+        {generos &&
+          generos.movies.map((genero, index) => (
+            <Carousel
+              key={`movies-${index}-${genero.name}`}
+              genre_id={genero.id}
+              textHeader={`Películas ${
+                index !== 0 && index % 3 === 0 ? "populares" : ""
+              } de ${genero.name}`}
+              isSerie={false}
+              isPopular={index !== 0 && index % 3 === 0}
+            />
+          ))}
+      </div>
+    </>
+  );
+};
